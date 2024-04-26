295596be71ba5e14305b3165b8ac9ef391b0fafb
Uday Sankar <udaysankar.ambadi@gmail.com>
2024-04-25 22:59:58 -0400

    tkinter main window setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Sorting Algorithms 
+
+
+## Create new conda environment
+`conda create --name sorting`
+`conda activate sorting`
+
+## Install required packages
+`pip install -r requirements.txt`
+
---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+
+#main window setup
+root = tk.Tk()
+root.title("Sorting Algorithm Visualizer")
+
+# frame for the controls
+frame = ttk.Frame(root)
+frame.pack(side=tk.TOP)
+
+root.mainloop()
